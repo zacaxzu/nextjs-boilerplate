a59c101cc82f9d57ea4b68003fc0ab4da663c554
zacaxzu <zeljka.baca1@gmail.com>
2025-12-11 22:56:08 +0100

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Test
           </a>
         </div>
       </main>
